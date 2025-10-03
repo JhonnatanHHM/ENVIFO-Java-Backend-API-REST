a394b1735f0eeb5d19164ce4116e389e19865715
Jhonnatan H. Henao M. <hhmjhonatan@gmail.com>
2025-10-03 14:43:18 -0500

    Cors Versel

--- src/main/java/com/envifo_backend_java/Envifo_backend_java/infrastructure/config/SecurityConfig.java
+++ src/main/java/com/envifo_backend_java/Envifo_backend_java/infrastructure/config/SecurityConfig.java
@@ -56,6 +56,7 @@ public class SecurityConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://envifo.vercel.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
